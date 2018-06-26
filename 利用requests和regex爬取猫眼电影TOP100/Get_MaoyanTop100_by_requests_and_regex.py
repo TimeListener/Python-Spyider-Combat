5aadabace8388e4936b90c56e5cbb63a575c1d2d
Newbie-97 <xuyaoyao8657945@qq.com>
2018-06-26 23:54:18 +0800

    代码修改23:54
    经过测试，可以正常爬取MaoyanTOP100相关影片信息。

--- 利用requests和regex爬取猫眼电影TOP100/Get_MaoyanTop100_by_requests_and_regex.py
+++ 利用requests和regex爬取猫眼电影TOP100/Get_MaoyanTop100_by_requests_and_regex.py
@@ -10,68 +10,65 @@ Created on Tue Jun 26 11:35:42 2018
 import requests
 import re
 
-#计算下运行时间
+# 计算下运行时间
 import time
-#没有使用xxx时，同一网速，用时1.7s
+# 没有使用xxx时，同一网速，用时1.7s
 
 from multiprocessing import Pool
 from requests.exceptions import RequestException
-
-
+import json
 
 header = {
-        'User-Agent':'Baiduspider+'
-        }
+    'User-Agent': 'Baiduspider+'
+}
 
-def getHTML(url,code='utf-8'):
+
+def getHTML(url, code='utf-8'):
     try:
-        r = requests.get(url,headers=header)
+        r = requests.get(url, headers=header)
         if r.status_code == 200:
             r.encoding = code
             return r.text
-        else :
+        else:
             print('getHTML Error')
     except RequestException:
         print('getHTML Error')
-            
+
+
 def parsePage(html):
     pattern = re.compile('.*?board-index-.*?">(.*?)</i>.*?class="name">.*?"boarditem-click".*?"{movieId:.*?}">(.*?)</a>.*?class="star">(.*?)</p>.*?class="releasetime">(.*?)</p>.*?<p class="score"><i class="integer">(.*?)</i><i class="fraction">(.*?)</i></p>',re.S)
-    items = re.findall(pattern,html)
-    
+    items = re.findall(pattern, html)
+
     for item in items:
         yield {
-                'index':item[0],
-                'Name':item[1],
-                'StarActor':item[2].strip(),
-                'ReleaseTime':item[3],
-                'Score':item[4]+item[5]
-                }
-        
+            'index': item[0],
+            'Name': item[1],
+            'StarActor': item[2].strip(),
+            'ReleaseTime': item[3],
+            'Score': item[4] + item[5]
+        }
+
+def write_page(item):
+    with open('result.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(item , ensure_ascii=False)  +  '\n')
+        f.close()
+
 def main(page):
-    url = 'http://maoyan.com/board/4?offset='+str(page)
+    url = 'http://maoyan.com/board/4?offset=' + str(page)
     html = getHTML(url)
     items = parsePage(html)
-    
     for item in items:
-        f.write(item)
-    
+        print(item)
+        write_page(item)
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     start = time.time()
-    
-    f = open('f://result.txt','a')
-    
+
     pool = Pool()
-    pool.map(main , [i*10 for i in range(10)])
+    pool.map(main, [i * 10 for i in range(10)])
     pool.close()
     pool.join()
-    
-    f.close()
-    
+
     end = time.time()
-    
-    print('It spends %s s'%(end-start))
-    
-    
-    
+
+    print('It spends %s s' % (end - start))
